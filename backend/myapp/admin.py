44a61bea7195b26f377b2f03f55873d73012c326
Sohail Patel <patelsohailp7@gmail.com>
2024-04-23 22:08:44 +0530

    req based code

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import * 
 
 admin.site.register(Project)
-admin.site.register(ProjectUser)
+admin.site.register(UserProject)
 admin.site.register(UserInterest)
+admin.site.register(JoinRequest)
 # Register models here.
